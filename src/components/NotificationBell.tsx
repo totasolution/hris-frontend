31612499e6e865cf44a1b7694fe1505c6a13b075
angripa.nadapdap <angripa.nadapdap@klikit.io>
2026-02-18 12:45:43 +0700

    hris enhancement

--- src/components/NotificationBell.tsx
+++ src/components/NotificationBell.tsx
@@ -28,7 +28,7 @@ export function NotificationBell() {
   useEffect(() => {
     loadNotifications();
     // Poll for new notifications every 30 seconds
-    const interval = setInterval(loadNotifications, 30000);
+    const interval = setInterval(loadNotifications, 300000);
     return () => clearInterval(interval);
   }, []);
 
